6ccec63b977665e7d561dda939443e795e3e8d23
nalule-grace <g.nalule@alustudent.com>
2023-10-18 18:46:25 +0200

    my site

---
+++ grace site/script.js
@@ -0,0 +1,19 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontent = document.getElementsByClassName("tab-content");
+function opentab(tabname){
+    for (tablinks of tablinks){
+        tablinks.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontent){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
